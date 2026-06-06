9d9789e8e35c5b4e8fd781e3c790deb85bcb6f85
ViktorLindstrm <viktor.lindstrm@gmail.com>
2026-06-06 20:16:09 +0200

    assets update

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,7 @@ import {LiveSocket} from "phoenix_live_view"
 import {hooks as colocatedHooks} from "phoenix-colocated/platser"
 import MapHook from "./hooks/map_hook"
 import topbar from "../vendor/topbar"
+import "maplibre-gl/dist/maplibre-gl.css"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
