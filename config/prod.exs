348851a47abeb7ebdcc6cbb15d69f0a3d4e58772
ViktorLindstrm <viktor.lindstrm@gmail.com>
2026-06-06 23:34:51 +0200

    remove https requirement

--- config/prod.exs
+++ config/prod.exs
@@ -10,14 +10,6 @@ config :platser, PlatserWeb.Endpoint, cache_static_manifest: "priv/static/cache_
 # Force using SSL in production. This also sets the "strict-security-transport" header,
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
-config :platser, PlatserWeb.Endpoint,
-  force_ssl: [
-    rewrite_on: [:x_forwarded_proto],
-    exclude: [
-      # paths: ["/health"],
-      hosts: ["localhost", "127.0.0.1"]
-    ]
-  ]
 
 # Configure Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req
